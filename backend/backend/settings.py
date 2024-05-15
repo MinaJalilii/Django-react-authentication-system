99ed6178092fa39c62e05290386e4fc52a922dd1
Mina <hominaa99@gmail.com>
2024-05-15 15:08:20 +0330

    update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nvijw5okpt=uaq+racs)(b_8wkn0jn8lm_3gu3&5^kke6=5*i@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-react-authentication-system.vercel.app/']
+ALLOWED_HOSTS = ['https://django-react-authentication-system.vercel.app']
 
 # Application definition
 
@@ -171,7 +171,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 CORS_ALLOWED_ORIGINS = [
-    'https://django-react-authentication-system.vercel.app/',
+    'https://django-react-authentication-system.vercel.app',
 ]
 
 # Optional: Allow credentials (cookies, authorization headers) to be included in CORS requests
